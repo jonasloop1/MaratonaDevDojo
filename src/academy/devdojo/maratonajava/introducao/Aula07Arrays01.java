ce8955053ca9b08a44004c507333434a6b214acf
Jonas <jonasloop1@gmail.com>
2023-09-29 18:15:51 -0300

    Arrays, tipos primitivos, referências, métodos.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int idade [] = new int[3];
+        idade[0] = 20;
+        idade[1] = 21;
+        idade[2] = 22;
+        System.out.println(idade[0]+" - "+idade[1]+" - "+idade[2]);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        //byte, short, int, long, float e double = 0
+        //char = \u0000 ou ' '
+        //boolean = false
+        //String = null
+        int teste [] = new int[3];
+        String teste1 [] = new String[3];
+        char teste2 [] = new char[3];
+        boolean teste3 [] = new boolean[3];
+
+        System.out.println("Valores primitivos: "+teste[0]+" - "+teste[1]+" - "+teste[2]);
+        System.out.println("Valores de referencias: "+teste1[0]+" - "+teste1[1]+" - "+teste1[2]);
+        System.out.println("Valores primitivos: "+teste2[0]+" - "+teste2[1]+" - "+teste2[2]);
+        System.out.println("Valores primitivos: "+teste3[0]+" - "+teste3[1]+" - "+teste3[2]+"\n");
+
+        String [] nomesArray = new String[3];
+
+        nomesArray [0] = "Goku";
+        nomesArray [1] = "Sasuke";
+        nomesArray [2] = "Luffy";
+
+        for (int i = 0; i < nomesArray.length; i++) {
+            System.out.println(nomesArray[i]);
+        }
+    }
+}
