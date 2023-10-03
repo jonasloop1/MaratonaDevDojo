b03cb7201e5ef5b3835f0285ed2c7d46ddb04f16
Jonas <jonasloop1@gmail.com>
2023-10-03 10:29:06 -0300

    Exercicio. mostrando para o usuario as informações básicas pessoais utilizando tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -2,12 +2,12 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula03TiposPrimitivosExercicios {
     public static void main(String[] args) {
-        String nome = "Jonas Silva";
+        String nome = "Jonas Silva Carvalho";
         String endereco = "Travessa piraque";
         String dataDeRecebimento = "20/09/2022";
         double salarioDouble = 2.000;
         String relatorio = "Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salario no valor de " + salarioDouble + " na data de " + dataDeRecebimento;
-
+        //formas diferente de mostrar na tela do usuario
         System.out.println(relatorio);
         System.out.println("Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salario no valor de " + salarioDouble + " na data de " + dataDeRecebimento);
     }
