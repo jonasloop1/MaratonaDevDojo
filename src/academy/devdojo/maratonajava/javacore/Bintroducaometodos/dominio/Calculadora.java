3fb57b5af296f670713c83d9abe5060640d96e7a
Jonas <jonasloop1@gmail.com>
2023-10-11 11:34:46 -0300

    Métodos, passagem de parâmetros e exemplos de retorno de métodos.

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -35,5 +35,28 @@ public class Calculadora {
             System.out.println(num1 / num2);
         }
     }
+    public void alteraDoisNumeros(int numero1, int numero2){
+        numero1 = 90;
+        numero2 = 100;
+        System.out.println("Dentro do AlteraDoisNumeros:");
+        System.out.println("Numero1: "+numero1);
+        System.out.println("Numero2: "+numero2);
+    }
+
+    public void somarArray(int[] numeros){
+        int soma = 0;
+        for (int num:numeros) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+
+    public void somaVarArgs(int... numeros2){
+        int soma = 0;
+        for (int num:numeros2) {
+            soma += num;
+        }
+        System.out.println(soma);
+    }
 }
 
