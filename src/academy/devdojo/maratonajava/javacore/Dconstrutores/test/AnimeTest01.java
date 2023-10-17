17406262117046b6effb523837f4c27adde9b584
Jonas <jonasloop1@gmail.com>
2023-10-17 17:08:56 -0300

    Construtores e Sobrecarga de construtores.

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("One piece", "Manga", 1200, "Ação", "Marvel");
+        anime.imprime();
+    }
+}
+
