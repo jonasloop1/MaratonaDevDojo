df9f52accd1ce5af6ab671a1fab91fd0100f7e2d
Jonas <jonasloop1@gmail.com>
2023-10-17 15:58:05 -0300

    Sobrecarga de métodos.

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.test;
+
+import academy.devdojo.maratonajava.javacore.Csobrecargametodos.diminio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+
+        anime.init("One piece", "Manga", 1200, "Ação");
+        anime.imprime();
+    }
+}
