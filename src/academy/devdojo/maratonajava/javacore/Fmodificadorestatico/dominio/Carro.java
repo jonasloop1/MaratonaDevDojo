d4ab98b9f36c3061167e2f0fd51101d628c6821d
Jonas <jonasloop1@gmail.com>
2023-10-28 17:54:05 -0300

    Modificador estatic.

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Carro.java
@@ -0,0 +1,44 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    public static double velocidadeLimite = 250;
+    //Modificador estatico afeta todos os objetos de uma classe, ou seja,
+    //aquele atributo especifico vai pertencer a classe!
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public void imprime(){
+        System.out.println("----------------");
+        System.out.println("Nome: "+this.nome);
+        System.out.println("Velocidade maxima: "+this.velocidadeMaxima+" KM");
+        System.out.println("Velocidade limite: "+Carro.velocidadeLimite+" KM");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+}
