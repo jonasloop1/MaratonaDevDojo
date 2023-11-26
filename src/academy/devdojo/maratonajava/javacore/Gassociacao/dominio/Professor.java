c775376427162703ef58fad58084277a7814d205
Jonas <jonasloop1@gmail.com>
2023-11-26 16:06:19 -0300

    Associação (Associação unidirecional muitos para um). Juntamente com exercicio sobre associação!

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Professor.java
@@ -2,9 +2,56 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Professor {
     private String nome;
+    private String especialidade;
+    private Seminario[] seminarios;
 
     public Professor(String nome) {
         this.nome = nome;
+    }
+
+    public Professor(String nome, String especialidade) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public Professor(String nome, String especialidade, Seminario[] seminarios) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.seminarios = seminarios;
+    }
+
+    public void imprime() {
+        System.out.println("\n**** Professores ****");
+        System.out.println("Nome do professor: " + this.nome);
+        System.out.println("Especialidade: " + this.especialidade);
+        if (this.seminarios == null) return;
+        System.out.println("\n#### Seminarios cadastrados ####");
+        for (Seminario seminario : this.seminarios) {
+            System.out.println("Titulo do seminario: " + seminario.getTitulo());
+            System.out.println("Endereço do seminario: " + seminario.getLocal().getEndereco());
+            if (seminario.getAlunos() == null || seminario.getAlunos().length == 0) continue;
+            System.out.println("\n**** ALunos ****");
+            for (Aluno aluno : seminario.getAlunos()) {
+                System.out.println("Aluno: " + aluno.getNome());
+                System.out.println("Idade: " + aluno.getIdade());
+            }
+        }
+    }
+
+    public Seminario[] getSeminarios() {
+        return seminarios;
+    }
+
+    public void setSeminarios(Seminario[] seminarios) {
+        this.seminarios = seminarios;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
     }
 
     public String getNome() {
