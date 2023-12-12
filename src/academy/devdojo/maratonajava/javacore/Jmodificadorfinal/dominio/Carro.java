7fef7ad6554b5e706b75e8a4922e7987c8ff3397
Jonas <jonasloop1@gmail.com>
2023-12-11 21:54:46 -0300

    Modificar final (tipo primirivo/tipo referência),aplicado para classes e métodos.

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Carro {
+    //Classes e métodos também podem ter o modificador final ex.: ' public final classe Carro(){} '
+    //Não pode subrescrever um método com modificado 'final'
+    private String nome;
+    //O modificador "final" defini uma variavel constante e uma palavra reservada.
+    //Geralmente as constantes são escritas com letra maisculas e underscore.
+    public static final double VELOCIDADE_LIMITE = 250;
+    public final Comprador COMPRADOR = new Comprador();
+
+    public final void imprime(){
+        System.out.println(this.nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Ferrari.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Ferrari extends Carro{
+    private String sachi;
+
+    public String getSachi() {
+        return sachi;
+    }
+
+    public void setSachi(String sachi) {
+        this.sachi = sachi;
+    }
+}
