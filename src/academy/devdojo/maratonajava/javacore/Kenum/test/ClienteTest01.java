8299c3eb1680b4b1312af2a922a54d742e22456a
Jonas <jonasloop1@gmail.com>
2023-12-13 09:17:45 -0300

    Enumeração.

---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Kenum.test;
+
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Douglas", TipoCliente.PESSOA_FISICA);
+        Cliente cliente2 = new Cliente("Embrapa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente3 = new Cliente("Junior", TipoCliente.PESSOA_FISICA);
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+        System.out.println(cliente3);
+    }
+}
