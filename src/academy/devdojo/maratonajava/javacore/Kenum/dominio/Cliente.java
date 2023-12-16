806e3cbaf976933c92fb456e460a58cbbd879d99
Jonas <jonasloop1@gmail.com>
2023-12-15 21:41:42 -0300

    Enumeração(Contrutores e atributos).

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -1,12 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public class Cliente {
+    public enum TipoPagamento{
+        DEBITO, CREDITO
+    }
     private String nome;
     private TipoCliente tipoCliente;
+    private TipoPagamento tipoPagamento;
 
-    public Cliente(String nome, TipoCliente tipoCliente) {
+    public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
+        this.tipoPagamento = tipoPagamento;
     }
 
     @Override
@@ -14,22 +19,8 @@ public class Cliente {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
                 ", tipoCliente=" + tipoCliente +
+                ", tipoClienteInt=" + tipoCliente.VALOR +
+                ", tipoPagamento=" + tipoPagamento +
                 '}';
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public TipoCliente getTipoCliente() {
-        return tipoCliente;
-    }
-
-    public void setTipoCliente(TipoCliente tipoCliente) {
-        this.tipoCliente = tipoCliente;
     }
 }
