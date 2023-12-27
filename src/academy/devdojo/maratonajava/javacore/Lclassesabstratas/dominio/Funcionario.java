dff91ebe0ff1dc57c90167f68da13fd01c606238
Jonas <jonasloop1@gmail.com>
2023-12-27 08:59:19 -0300

    Classes abstratas, métodos abstratos.

---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio;
+
+public abstract class Funcionario {
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+        calcularBonus();
+    }
+
+    public abstract void calcularBonus();
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.test;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+
+    }
+}
