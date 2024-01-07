a02c6e57d0f6f354b23707ec55ee5f2dbe9edb27
Jonas <jonasloop1@gmail.com>
2024-01-06 21:36:11 -0300

    Polimorfismo, parâmetros.

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProtudoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProtudoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProtudoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC10i7", 10000);
         Tomate tomate = new Tomate("Tomate verde", 20);
+        Televisao tv = new Televisao("Samsung 60\" ", 50000);
 
         CalculadoraImposto.calculadoraImposto(computador);
         System.out.println("------------------------");
         CalculadoraImposto.calculadoraImposto(tomate);
+        System.out.println("------------------------");
+        CalculadoraImposto.calculadoraImposto(tv);
     }
 }
