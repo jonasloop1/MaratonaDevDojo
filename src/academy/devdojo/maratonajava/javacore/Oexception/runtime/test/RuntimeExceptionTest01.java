234e7aefc510beb93af5bb9919bff3acab3a8762
Jonas <jonasloop1@gmail.com>
2024-01-26 18:27:35 -0300

    Throwable, Error, RuntimeException

--- src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         //Checked: É obrigado fazer a verificação! e Unchecked: não é obrigátorio fazer a verificação!
-
+        int[] nums = {1, 2};
+        System.out.println(nums[2]);
     }
 }
