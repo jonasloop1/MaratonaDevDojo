33f107699f245605958a8100cb6a524d106f3513
Jonas <jonasloop1@gmail.com>
2024-02-02 08:55:59 -0300

    Throwable, Error, RuntimeException,lançando exceções unchecked e checked.

--- src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTet02.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTet02.java
@@ -3,19 +3,19 @@ package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
 import java.io.File;
 import java.io.IOException;
 
-public class ExceptionTet01 {
-    public static void main(String[] args) {
+public class ExceptionTet02 {
+    public static void main(String[] args) throws IOException {
         criarNovoArquivo();
     }
 
-    public static void criarNovoArquivo() {
+    public static void criarNovoArquivo() throws IOException{
         File file = new File("arquivo\\teste.txt");
-
         try {
             boolean isCriado = file.createNewFile();
             System.out.println("Arquivo criado: " + isCriado);
         } catch (IOException e) {
             e.printStackTrace();
+            throw e;
         }
     }
 }
