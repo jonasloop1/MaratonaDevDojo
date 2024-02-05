4d21c366a48c3dfcab19bef691a572739ccac634
Jonas <jonasloop1@gmail.com>
2024-02-05 11:29:13 -0300

    Throwable, Error, RuntimeException,lançando exceções unchecked e checked.  Bloco Finally.

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest03 {
+    public static void main(String[] args) {
+        AbreConexao();
+    }
+
+    private static String AbreConexao() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "Conexão aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+        return null;
+    }//O bloco finally sempre será executado, independe se teve ou não uma exceção
+
+    private static void AbreConexao2() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            throw new RuntimeException();
+        } finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+    }
+}
