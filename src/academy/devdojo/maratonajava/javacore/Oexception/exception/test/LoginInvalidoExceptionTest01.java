2520406cc2cf5204aa0698889b15e2fd0dfbbae6
Jonas <jonasloop1@gmail.com>
2024-02-14 12:04:18 -0300

    Throwable, Error, RuntimeException,lançando exceções unchecked e checked. Bloco Finally. Multiplas exceções e multi catch em uma linha. Try with resources. Exceções customizadas.

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/LoginInvalidoExceptionTest01.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.LoginInvalidoException;
+
+import java.util.Scanner;
+
+public class LoginInvalidoExceptionTest01 {
+    public static void main(String[] args) {
+        try {
+            logar();
+        } catch (LoginInvalidoException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+    private static void logar() throws LoginInvalidoException {
+        Scanner teclado = new Scanner(System.in);
+
+        String userNameDB = "Goku";
+        String senhaDB = "SSJ";
+
+        System.out.println("Usuário:");
+        String userNameDigitado = teclado.nextLine();
+        System.out.println("Senha:");
+        String senhaDigitado = teclado.nextLine();
+
+        if (!userNameDB.equals(userNameDigitado) || !senhaDB.equals(senhaDigitado)){
+            throw new LoginInvalidoException("Usuário ou senha inválido");
+        }
+        System.out.println("Usuário logado com sucesso");
+    }
+}
