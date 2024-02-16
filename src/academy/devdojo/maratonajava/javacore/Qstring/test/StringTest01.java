5773f27c5caea8fdfe05cb5121c07f739663d88e
Jonas <jonasloop1@gmail.com>
2024-02-16 10:17:24 -0300

    Classes Utilitárias - Strings

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        //Strings são imutáveis!!!
+        String nome = "jonas"; //String pool (piscina de String)
+        String nome2 = "jonas";
+
+        nome = nome.concat(" silva");
+
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+
+        String nome3 = new String("jonas"); //1 variavel de referência, 2 objeto do tipo string, 3 uma string no pool de String
+        System.out.println(nome3 == nome2);
+        System.out.println(nome3.intern() == nome2);//intern() método que retornar uma representação canonica do objeto, e pega a referencia dentro do pool de String
+    }
+}
