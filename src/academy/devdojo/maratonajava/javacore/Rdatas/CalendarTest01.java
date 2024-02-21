ea5e573809dba93304c48c4b4e9b58aec16faa48
Jonas <jonasloop1@gmail.com>
2024-02-21 18:11:28 -0300

    Classes Utilitárias - Date; Caledar;

--- src/academy/devdojo/maratonajava/javacore/Rdatas/CalendarTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/CalendarTest01.java
@@ -9,13 +9,13 @@ public class CalendarTest01 {
         //Classe abstrata, não pode ser instânciada com o NEW!
         Calendar c = Calendar.getInstance();
 
-        if(c.getFirstDayOfWeek() == Calendar.SUNDAY){
+        if (c.getFirstDayOfWeek() == Calendar.SUNDAY) {
             System.out.println("Domingão é o primeiro dia da semana!!");
         }
-        System.out.println("Primeiro dia da semana: "+c.get(Calendar.DAY_OF_WEEK));
-        System.out.println("Primeiro dia do mês: "+c.get(Calendar.DAY_OF_MONTH));
-        System.out.println("Primeiro dia do ano: "+c.get(Calendar.DAY_OF_YEAR));
-        System.out.println("Primeiro dia da semana no mês: "+c.get(Calendar.DAY_OF_WEEK_IN_MONTH)+"\n");
+        System.out.println("Primeiro dia da semana: " + c.get(Calendar.DAY_OF_WEEK));
+        System.out.println("Primeiro dia do mês: " + c.get(Calendar.DAY_OF_MONTH));
+        System.out.println("Primeiro dia do ano: " + c.get(Calendar.DAY_OF_YEAR));
+        System.out.println("Primeiro dia da semana no mês: " + c.get(Calendar.DAY_OF_WEEK_IN_MONTH) + "\n");
 
         c.add(Calendar.DAY_OF_MONTH, 2);
         c.add(Calendar.HOUR, 2);
