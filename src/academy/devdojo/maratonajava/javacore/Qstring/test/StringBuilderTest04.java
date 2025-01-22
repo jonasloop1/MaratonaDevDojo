8118e84e13166210b05fadce2170a40ef5a8676b
Jonas <jonasloop1@gmail.coim>
2025-01-22 16:53:11 -0300

    Classes Utilitárias - Explorando métodos StringBuilder.

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest04.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest04.java
@@ -3,17 +3,19 @@ package academy.devdojo.maratonajava.javacore.Qstring.test;
 public class StringBuilderTest04 {
     public static void main(String[] args) {
         String nome = "jonas silva";
-        nome.concat(" carvalho");
-        nome.substring(0, 3);
-        System.out.println(nome);
+        nome = nome.concat(" carvalho"); // Para ver a mudança tem que ser feita a referencia, como nesse exemplo!
+        nome.substring(0, 3); // Aqui foi criada outra string em memoria pegandoa posição 0,1,2.
+        System.out.println("\n" + nome + "\n");
 
+        //StringBuilder é uma classe que trabalha com as String
         StringBuilder sb = new StringBuilder("jonas silva");
         sb.append(" carvalho").append(" academy");
+        sb.reverse(); // Inverte a ordem da String
         sb.reverse();
-        sb.reverse();
-        sb.delete(0, 3);
-        String substring = sb.substring(0, 2);
+        sb.delete(0, 3); // Apaga os valores de acordo com o indice passado
         System.out.println(sb);
+
+        String substring = sb.substring(0, 15); //Criando outro objeto com os valores do sb
         System.out.println(substring);
     }
 }
