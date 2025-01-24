596817f37e9409ff54f959b1140ec918ea189333
Jonas <jonasloop1@gmail.coim>
2025-01-23 22:05:28 -0300

    Classes Utilitárias - Classe Caledar e seus métodos;

--- src/academy/devdojo/maratonajava/javacore/Rdatas/CalendarTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/CalendarTest01.java
@@ -1,5 +1,9 @@
 package academy.devdojo.maratonajava.javacore.Rdatas;
-
+/*
+ * @Jonas Silva
+ * @Jonasloop1@gmail.com
+ *
+ * */
 import java.util.Calendar;
 import java.util.Date;
 
@@ -9,7 +13,7 @@ public class CalendarTest01 {
         //Classe abstrata, não pode ser instânciada com o NEW!
         Calendar c = Calendar.getInstance();
 
-        if (c.getFirstDayOfWeek() == Calendar.SUNDAY) {
+        if (c.getFirstDayOfWeek() == Calendar.SUNDAY) { //Pegar o primeiro dia da semana
             System.out.println("Domingão é o primeiro dia da semana!!");
         }
         System.out.println("Primeiro dia da semana: " + c.get(Calendar.DAY_OF_WEEK));
@@ -17,8 +21,8 @@ public class CalendarTest01 {
         System.out.println("Primeiro dia do ano: " + c.get(Calendar.DAY_OF_YEAR));
         System.out.println("Primeiro dia da semana no mês: " + c.get(Calendar.DAY_OF_WEEK_IN_MONTH) + "\n");
 
-        c.add(Calendar.DAY_OF_MONTH, 2);
-        c.add(Calendar.HOUR, 2);
+        c.add(Calendar.DAY_OF_MONTH, 2); //Adiciona 2 dias na data atual do sistema
+        c.add(Calendar.HOUR, 2); //Adiciona horas no horário atual do sistema
 
         Date date = c.getTime();
         System.out.println(date);
