23aa2d1297a8b2aee3ed1aa20e02b634be1fa0bc
Jonas <jonasloop1@gmail.coim>
2025-03-12 15:26:18 -0300

    Classes Utilitárias - IO pt 02 - FileWriter.

--- src/academy/devdojo/maratonajava/javacore/Vio/FileWriterTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/FileWriterTest01.java
@@ -1,7 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Vio;
 /*
- * @Jonas Silva
- * @Jonasloop1@gmail.com
+ * @author Jonas Silva
+ * @e-mail: Jonasloop1@gmail.com
  * @param Entrada e saída de dados.
  * Classe fileWriter e os seus métodos.
  * */
@@ -12,10 +12,10 @@ import java.io.IOException;
 
 public class FileWriterTest01 {
     public static void main(String[] args) {
-        File file = new File("arquivo.txt");
+        File file = new File("file.txt");
 
         try (FileWriter fw = new FileWriter(file, true)) {
-            fw.write("O melhor curso de DESENVOLVIMENTO do Mundo!\nO melhor curso de DESENVOLVIMENTO do Mundo!");
+            fw.write("O melhor curso de DESENVOLVIMENTO do Mundo!\nDevDojo o melhor curso de DESENVOLVIMENTO do Mundo!\n");
             fw.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
