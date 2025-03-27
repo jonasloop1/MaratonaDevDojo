29a7a4d0b8237c2b04d1ad595a0dee4274c9e9a7
Jonas <jonasloop1@gmail.coim>
2025-03-27 20:30:38 -0300

    Classes Utilitárias - NIO pt 01 - Path, Paths, Files pt 01 - atualização

--- src/academy/devdojo/maratonajava/javacore/Wnio/PathTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Wnio/PathTest01.java
@@ -3,7 +3,11 @@ package academy.devdojo.maratonajava.javacore.Wnio;
  * @author Jonas Silva
  * @e-mail: Jonasloop1@gmail.com
  * @param Entrada e saída de dados.
- * Trabalhando com a classe NIO, path, paths e files.
+ * Trabalhando com a classe NIO, path, paths e files;
+ * A interface da path foi criada para substituir a file;
+ * Para criar um path você vai precisar usar a classe paths;
+ * E ainda existe a classe Files, que não tem nada relacionado com a File;
+ * A classe Files tem relação com a classe paths.
  *
  * */
 
@@ -12,6 +16,7 @@ import java.nio.file.Paths;
 
 public class PathTest01 {
     public static void main(String[] args) {
+        //Diferentes formas de encontrar um arquivo
         Path p1 = Paths.get("C:\\Users\\JonasJoao\\Documents\\DevDojo\\MaratonaDevDojo\\file.txt");
         Path p2 = Paths.get("C:\\Users\\JonasJoao\\Documents\\DevDojo\\MaratonaDevDojo", "file.txt");
         Path p3 = Paths.get("C:", "Users", "JonasJoao\\Documents\\DevDojo\\MaratonaDevDojo", "file.txt");
