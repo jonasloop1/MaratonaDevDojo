0b2294db2ec30d45dbbc18325075930ec32ff6ca
Jonas <jonasloop1@gmail.coim>
2025-04-01 16:30:27 -0300

    Classes Utilitárias - NIO pt 07 - BasicFileAttributes pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/BasicFileAttributesTest02.java
@@ -0,0 +1,46 @@
+package academy.devdojo.maratonajava.javacore.Wnio;
+/*
+ * @author Jonas Silva
+ * @e-mail: Jonasloop1@gmail.com
+ * @param Entrada e saída de dados.
+ * Classes que terminam com VIEW no final, dá a possibilidade de alterar alguns atributos.
+ * BasicFileAttributes -> interface geral
+ * DosFileAttributes -> Implementação
+ * PosixFileAttributes -> voltado para sistemas Unix
+ *
+ * */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+
+public class BasicFileAttributesTest02 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("pasta/new.txt");
+        BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+
+        FileTime creationTime = basicFileAttributes.creationTime();
+        FileTime lastModifiedTime = basicFileAttributes.lastModifiedTime();
+        FileTime lastAccessTime = basicFileAttributes.lastAccessTime();
+
+        System.out.println("creationTime: " + creationTime);
+        System.out.println("lastModifiedTime: " + lastModifiedTime);
+        System.out.println("lastAccessTime: " + lastAccessTime);
+
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        FileTime newCreationTime = FileTime.fromMillis(System.currentTimeMillis());
+        fileAttributeView.setTimes(lastModifiedTime, newCreationTime, creationTime);
+        System.out.println("------------------------------------------- ");
+        creationTime = fileAttributeView.readAttributes().creationTime();
+        lastModifiedTime = fileAttributeView.readAttributes().lastModifiedTime();
+        lastAccessTime = fileAttributeView.readAttributes().lastAccessTime();
+
+        System.out.println("creationTime: " + creationTime);
+        System.out.println("lastModifiedTime: " + lastModifiedTime);
+        System.out.println("lastAccessTime: " + lastAccessTime);
+
+    }
+}
