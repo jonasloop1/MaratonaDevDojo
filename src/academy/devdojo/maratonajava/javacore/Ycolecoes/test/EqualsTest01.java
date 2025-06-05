85611f07c962e5a300ab677eb6cfca87dd3d2d28
Jonas <jonasloop1@gmail.coim>
2025-06-05 20:43:57 -0300

    Classes Utilitárias - equals  pt 01 e pt 02.

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/EqualsTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+/*
+ * @author Jonas Silva
+ * @e-mail: Jonasloop1@gmail.com
+ * @param Coleções.
+ *
+ * */
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1CD2FC", "Samsung");
+        Smartphone s2 = s1;
+
+        System.out.println(s1.equals(s2));
+    }
+}
