10c568670f1ef78e7fa3543b2a3b1788f81100c6
Jonas <jonasloop1@gmail.coim>
2025-08-18 16:46:45 -0300

    Coleções - Sorting - usando Comparable e Comparator.

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Manga.java
@@ -12,6 +12,7 @@ public class Manga implements Comparable<Manga> {
     private Long id;
     private String nome;
     private double preco;
+    private int quantidade;
 
     public Manga(Long id, String nome, double preco) {
         Objects.requireNonNull(id, "Id não pode ser nullo!");
@@ -19,6 +20,11 @@ public class Manga implements Comparable<Manga> {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
+    }
+
+    public Manga(Long id, String nome, double preco, int quantidade) {
+        this(id,nome,preco);
+        this.quantidade = quantidade;
     }
 
     @Override
@@ -39,6 +45,7 @@ public class Manga implements Comparable<Manga> {
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", preco=" + preco +
+                ", quantidade=" + quantidade +
                 '}';
     }
 
@@ -64,6 +71,14 @@ public class Manga implements Comparable<Manga> {
 
     public void setPreco(double preco) {
         this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 
     @Override
